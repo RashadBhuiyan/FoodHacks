a224495445bd1441801cd87ed5815a03eb602078
Rashad Ahmed Bhuiyan <rashadabhuiyan@gmail.com>
2020-01-25 13:28:22 -0500

    Preliminary Files for Web Application
    These are just the preliminary files I created for building the web application. The CSS is just copy-paste from previous code and can be completely replaced with the styling that we desire.

---
+++ flow.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCxB8D_4Oap4WKG7z7emq7XlR9CnjJUXDE",
+  authDomain: "foodhacks-48218.firebaseapp.com",
+  databaseURL: "https://foodhacks-48218.firebaseio.com",
+  projectId: "foodhacks-48218",
+  storageBucket: "foodhacks-48218.appspot.com",
+  messagingSenderId: "509970892298",
+  appId: "1:509970892298:web:5db005252ea4ba808941f3",
+  measurementId: "G-7KQRK4QDGN"
+};
